23e272d76a1526fe2cd1ca6397873aebbb365784
mturan01 <mehmetculha96@gmail.com>
2023-04-21 02:53:41 +0300

    ortam değişkenleri

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from "react";
 import logo from "./logo.svg"
 
 function App() {
+  useEffect(()=>{
+    if(process.env.NODE_ENV=='production'){
+      // google analisti bilgilendir
+    }
+  })
   return (
     <div className="App">
+ <h3>{process.env.NODE_ENV}</h3>
+ <p>{process.env.REACT_APP_API_URL}</p>
+ {process.env.NODE_ENV=='production' &&(
+  <>
  <img src="logo192.png" alt="" />
  <img src={logo} alt="" />
+ </>
+ )}
     </div>
   );
 }
